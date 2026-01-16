b6e9b561c315fef0e4420ab0d7e67d1216f0cc7d
Kevin J <kevinjia05@gmail.com>
2026-01-16 09:48:33 -0800

    Initial Commit: Instructor given discussion files

---
+++ testsrc/StringUtilsTest.cpp
@@ -0,0 +1,62 @@
+#include <gtest/gtest.h>
+#include "StringUtils.h"
+
+TEST(StringUtilsTest, SliceTest){
+    
+}
+
+TEST(StringUtilsTest, Capitalize){
+    EXPECT_EQ(StringUtils::Capitalize("hello"),std::string("Hello"));
+}
+
+TEST(StringUtilsTest, Upper){
+    
+}
+
+TEST(StringUtilsTest, Lower){
+    
+}
+
+TEST(StringUtilsTest, LStrip){
+    
+}
+
+TEST(StringUtilsTest, RStrip){
+    
+}
+
+TEST(StringUtilsTest, Strip){
+    
+}
+
+TEST(StringUtilsTest, Center){
+    
+}
+
+TEST(StringUtilsTest, LJust){
+    
+}
+
+TEST(StringUtilsTest, RJust){
+    
+}
+
+TEST(StringUtilsTest, Replace){
+    
+}
+
+TEST(StringUtilsTest, Split){
+    
+}
+
+TEST(StringUtilsTest, Join){
+    
+}
+
+TEST(StringUtilsTest, ExpandTabs){
+    
+}
+
+TEST(StringUtilsTest, EditDistance){
+    
+}
